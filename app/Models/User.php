8b0f41ca9c685d0fe57d3df87d12f456bce1ae4e
Andres Pizo <pizoluligoa@gmail.com>
2024-10-04 16:34:15 -0500

    Add relations person in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -75,4 +76,10 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function persona(): BelongsTo
+    {
+        return $this->belongsTo(Persona::class, 'idPersona');
+    }
+
 }
