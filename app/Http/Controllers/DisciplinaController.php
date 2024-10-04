e29aae4446e6f74893b84b351b33115f60890f5d
Andres Pizo <pizoluligoa@gmail.com>
2024-10-04 16:20:55 -0500

    add new table of disciplinas

---
+++ app/Http/Controllers/DisciplinaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Disciplina;
+use Illuminate\Http\Request;
+
+class DisciplinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Disciplina $disciplina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Disciplina $disciplina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Disciplina $disciplina)
+    {
+        //
+    }
+}
---
+++ app/Models/Disciplina.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Disciplina extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = ['id'];
+
+    public function setCreatedAtAttribute($value): void
+    {
+        date_default_timezone_set("America/Bogota");
+        $this->attributes['created_at'] = Carbon::now();
+    }
+
+    public function setUpdatedAtAttribute($value): void
+    {
+        date_default_timezone_set("America/Bogota");
+        $this->attributes['updated_at'] = Carbon::now();
+    }
+}
