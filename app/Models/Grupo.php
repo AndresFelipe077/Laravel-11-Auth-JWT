86725ce65347c498f7efdd7cdeb3fd98eac1f034
Andres Pizo <pizoluligoa@gmail.com>
2024-10-04 16:01:59 -0500

    add new table of grupos

---
+++ app/Models/Grupo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Grupo extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = ['id'];
+
+    public function setCreatedAtAttribute($value): void
+    {
+        date_default_timezone_set("America/Bogota");
+        $this->attributes['created_at'] = Carbon::now();
+    }
+
+    public function setUpdatedAtAttribute($value): void
+    {
+        date_default_timezone_set("America/Bogota");
+        $this->attributes['updated_at'] = Carbon::now();
+    }
+
+    public function torneo(): BelongsTo
+    {
+        return $this->belongsTo(Torneo::class, 'idTorneo');
+    }
+
+}
